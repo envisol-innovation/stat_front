7cdc86dca37951f2402f897962777000b33d5c78
Victorine HERBIN <v.herbin@envisol.fr>
2024-11-18 17:22:09 +0100

    Test ajout sur branche dev ?

--- pages/index.vue
+++ pages/index.vue
@@ -33,6 +33,30 @@
   <div v-if="img_swarmplot != ''">
     <img v-bind:src="`data:image/jpg;base64,${img_swarmplot}`" />
   </div>
+
+  <div> Box plot </div>
+  
+  <!-- <v-select v-model="list_elements" :items="colonnes" label="Éléments à analyser"> </v-select> -->
+  <v-select v-model="boxplot_sum_element" :items="colonnes" label="Somme du type d'éléments à analyser"> </v-select>
+  <v-select v-model="selected_list_elements" :items="colonnes" label="Éléments à analyser" multiple>
+    <template v-slot:prepend-item>
+      <v-list-item @click="toggle">
+        <v-list-item-action>
+          <v-icon :color="selected_list_elements.length > 0 ? 'indigo darken-4' : ''"></v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>
+            Selectionner tous
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider class="mt-2"></v-divider>
+    </template>
+  </v-select>
+  <v-btn color="success" @click="post_boxplot">Boxplot !</v-btn>
+  <div v-if="img_boxplot != ''">
+    <img v-bind:src="`data:image/jpg;base64,${img_boxplot}`" />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -93,6 +117,12 @@ let swarmplot_nom_classifier = ref("");
 let img_swarmplot = ref("")
 watch(img_swarmplot, () => {console.log("hello new img?")})
 
+let selected_list_elements = ref([""]);
+let boxplot_sum_element = ref("");
+let img_boxplot = ref("")
+watch(img_boxplot, () => {console.log("hello new box img?")})
+
+
 async function post_swarmplot() {
   console.log("Swarmplot !!!", data_csv[0]);
   const { data: res } = await useFetch(bck_end_base_url_+'/EDASwarmPlot', {
@@ -100,7 +130,11 @@ async function post_swarmplot() {
     method: 'POST',
     body: {"dataframe": data_csv, "swarmplot_nom_classifier": swarmplot_nom_classifier, "swarmplot_nom_elem": swarmplot_nom_elem},
     onResponse({ request, response, options }) {
-      img_swarplot.value = response._data["fig"];
+    // Process the response data
+    // img_swarmplot = response
+    // console.log("104 response", response);
+    // console.log("104 response", response._data["fig"]);
+    img_swarmplot.value = response._data["fig"];
     },
     onResponseError({ request, response, options }) {
       // Handle the response errors
