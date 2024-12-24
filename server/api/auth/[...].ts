2cdda98a5ab8707ab3d1d3d10ced17c0d2337902
JustineELIAS <j.elias@envisol.fr>
2024-12-24 11:19:19 +0100

    added only the nuct auth config

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,45 +1,41 @@
-// import { NuxtAuthHandler } from '#auth'
-// import Auth0Provider from "next-auth/providers/auth0";
+import { NuxtAuthHandler } from '#auth'
+import Auth0Provider from "next-auth/providers/auth0";
 
 
-// export default NuxtAuthHandler({
-//     // A secret string you define, to ensure correct encryption
-//     secret: useRuntimeConfig().authSecret,
-//     providers: [
-//         // GithubProvider.default({
-//         //     clientId: 'your-client-id',
-//         //     clientSecret: 'your-client-secret'
-//         // }),
-//         Auth0Provider.default({
-//             clientId: useRuntimeConfig().client_id,
-//             clientSecret: useRuntimeConfig().client_secret,
-//             issuer: useRuntimeConfig().issuer,    // this must be the full url (with https://)
-//             wellKnown: useRuntimeConfig().well_known   // this is necessary for some obscure reason https://github.com/nextauthjs/next-auth/issues/7591
-//           })
-//     ],
-//     // callbacks: {
-//     //     jwt({ token, account, profile }) {
-//     //       if (account) {
-//     //         token.sessionToken = account.session_token
-//     //       }
-//     //       return token
-//     //     },
-//     //     // async session({ session, token }) {
-//     //     //     // Token we injected into the JWT callback above.
-//     //     //     const token = token.sessionToken
+export default NuxtAuthHandler({
+    // A secret string you define, to ensure correct encryption
+    secret: process.env.AUTH0_SECRET,
+    providers: [
+        Auth0Provider.default({    // .default() is correct from the doc
+            clientId: process.env.AUTH0_CLIENT_ID,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET,
+            issuer: "https://" + process.env.AUTH0_DOMAIN,    // this must be the full url (with https://)
+            wellKnown: "https://" + process.env.AUTH0_DOMAIN + ".well-known/openid-configuration"   // this is necessary for some obscure reason https://github.com/nextauthjs/next-auth/issues/7591
+          })
+    ],
+    // callbacks: {
+    //     jwt({ token, account, profile }) {
+    //       if (account) {
+    //         token.sessionToken = account.session_token
+    //       }
+    //       return token
+    //     },
+    //     // async session({ session, token }) {
+    //     //     // Token we injected into the JWT callback above.
+    //     //     const token = token.sessionToken
       
-//     //     //     // Fetch data OR add previous data from the JWT callback.
-//     //     //     const additionalUserData = await $fetch(`/api/session/${token}`)
+    //     //     // Fetch data OR add previous data from the JWT callback.
+    //     //     const additionalUserData = await $fetch(`/api/session/${token}`)
       
-//     //     //     // Return the modified session
-//     //     //     return {
-//     //     //       ...session,
-//     //     //       user: {
-//     //     //         name: additionalUserData.name,
-//     //     //         avatar: additionalUserData.avatar,
-//     //     //         role: additionalUserData.role
-//     //     //       }
-//     //     //     }
-//     //     //   },
-//     // }
-// })
+    //     //     // Return the modified session
+    //     //     return {
+    //     //       ...session,
+    //     //       user: {
+    //     //         name: additionalUserData.name,
+    //     //         avatar: additionalUserData.avatar,
+    //     //         role: additionalUserData.role
+    //     //       }
+    //     //     }
+    //     //   },
+    // }
+})
